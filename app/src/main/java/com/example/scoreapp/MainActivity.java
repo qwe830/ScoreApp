82c2388f613ffb513e0177b2006f2a22003e285f
这把乱杀 <1738682940@qq.com>
2021-05-25 17:46:41 +0800

    优化代码

--- app/src/main/java/com/example/scoreapp/MainActivity.java
+++ app/src/main/java/com/example/scoreapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.scoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,14 +9,21 @@ import com.example.scoreapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
-        private int scoreA, scoreB, saveA, saveB;
-        private final String KEY = "key";
-        private final String MY_SP = "my_sp", SP_A = "sp_a", SP_B = "sp_b";
-        @Override
+        private int scoreA, scoreB;
+        private int saveA,saveB;
+    private final String KEY = "key";
+    private final String MY_SP = "my_sp", SP_A = "sp_a", SP_B = "sp_b";
+
+    public MainActivity() {
+    }
+
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             mBinding = ActivityMainBinding.inflate(getLayoutInflater());
             setContentView(mBinding.getRoot());
+
 
             mBinding.buttonAAdd1.setOnClickListener(v -> {
                 addA(1);
@@ -35,9 +43,32 @@ public class MainActivity extends AppCompatActivity {
             mBinding.buttonBAdd3.setOnClickListener(v -> {
                 addB(3);
             });
+            mBinding.imageButtonRevoke.setOnClickListener(v -> {
+               reset();
+            });
+           mBinding.imageButtonReset.setOnClickListener(v ->{
+               revoke();
+           });
+
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray(KEY, new int[]{scoreA, scoreB, saveA, saveB});
+    }
+
+    private void revoke() {
+        scoreA = saveA;
+        scoreB = saveB;
+
+        show();
+    }
 
 
-
+    private void reset() {
+        scoreA = 0;
+        scoreB = 0;
+        show();
     }
 
     private void addB(int i) {
@@ -49,8 +80,17 @@ public class MainActivity extends AppCompatActivity {
         show();
         scoreA += i;
     }
+
     public void show() {
         mBinding.textScoreA.setText(String.valueOf(scoreA));
         mBinding.textScoreB.setText(String.valueOf(scoreB));
+    }
+
+    public void setSaveA(int saveA) {
+        this.saveA = saveA;
+    }
+
+    public void setSaveB(int saveB) {
+        this.saveB = saveB;
     }
 }
